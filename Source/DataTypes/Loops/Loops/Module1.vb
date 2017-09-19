935ac40f90548c3af9550bf0277693d683f8bf4d
MADU\Imad Ud Din Khan <emaduddin181@gmail.com>
2017-09-20 03:12:37 +0500

    For advanced

--- Source/DataTypes/Loops/Loops/Module1.vb
+++ Source/DataTypes/Loops/Loops/Module1.vb
@@ -5,6 +5,7 @@
 
         'Loops
         ' While Loop
+        Console.WriteLine("   While Loop   ")
         While a <= 15.0
 
             a += 1
@@ -12,18 +13,18 @@
             If a >= 4 And a < 6 Then
                 Continue While
             End If
-            Console.Write(a.ToString & ",")
+            Console.Write(a.ToString & "  ")
             If a = 12 Then
                 Exit While
             End If
         End While
 
-        Console.WriteLine("")
+        Console.WriteLine(vbCrLf & vbCrLf & "   For Loop   ")
 
         'For Loop
-        For num As Double = 0 To 10
+        For num As Double = 3 To 0 Step -0.5
 
-            Console.Write(num.ToString & ",")
+            Console.Write(num.ToString & "  ")
 
         Next
         Console.Read()
