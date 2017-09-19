5b0ec82557ca5dc3de71821dd94b87f8cc723b8e
MADU\Imad Ud Din Khan <emaduddin181@gmail.com>
2017-09-20 03:05:44 +0500

    While & For Loop

---
+++ Source/DataTypes/Loops/Loops/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a As Integer = 0
+
+        'Loops
+        ' While Loop
+        While a <= 15.0
+
+            a += 1
+
+            If a >= 4 And a < 6 Then
+                Continue While
+            End If
+            Console.Write(a.ToString & ",")
+            If a = 12 Then
+                Exit While
+            End If
+        End While
+
+        Console.WriteLine("")
+
+        'For Loop
+        For num As Double = 0 To 10
+
+            Console.Write(num.ToString & ",")
+
+        Next
+        Console.Read()
+    End Sub
+
+End Module
