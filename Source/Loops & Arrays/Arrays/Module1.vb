fbab158467f8c855c59aa5b4c164430b10180e10
MADU\Imad Ud Din Khan <emaduddin181@gmail.com>
2017-09-20 16:15:11 +0500

    loop basics on arrays

---
+++ Source/Loops & Arrays/Arrays/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num(3) As Integer
+
+        num(0) = 10
+        num(1) = 2
+        num(2) = 4
+        num(3) = 1
+
+
+        For i = 0 To num.Length - 1 Step 1
+            Console.WriteLine(num(i))
+        Next
+
+
+        For Each n In num
+            Console.WriteLine(n)
+        Next
+
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
